38f2a0ae3087e92b95bb248f80f9a808caf242de
Abdulrahman-4-alx <codemywebinar@gmail.com>
2023-08-10 18:57:13 -0700

    New commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Doing hard things 
+ * Return: 0 (Workdout)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
